3dac5fbb22d89864d52ba439e56d033cb0bbce56
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-12-13 12:00:12 +0100

    Update changelog.sh

--- changelog.sh
+++ changelog.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -l
+#!/bin/sh
 
 github_changelog_generator -u $1 -p $2 -o --release-branch $3 -o CHANGELOG.md
 
-changelog=$(cat ./CHANGELOG.md)
+changelog=`cat CHANGELOG.md`
 echo ::set-output name=changelog::$changelog
